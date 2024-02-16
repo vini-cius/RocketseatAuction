bf7a9284e0dcbc8d35ad55e29c452bdf2cfed567
Vinicius de Souza Santos <viniciuskt0@gmail.com>
2024-02-15 23:03:39 -0300

    Add project files.

---
+++ src/RocketseatAuction.API/Repositories/RocketseatAuctionDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RocketseatAuction.API.Entities;
+
+namespace RocketseatAuction.API.Repositories;
+
+public class RocketseatAuctionDbContext : DbContext
+{
+	public DbSet<Auction> Auctions { get; set; }
+
+	protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+	{
+		optionsBuilder.UseSqlite("Data Source=C:\\Users\\souza\\OneDrive\\Documentos\\Development\\leilaoDbNLW.db");
+
+	}
+}
